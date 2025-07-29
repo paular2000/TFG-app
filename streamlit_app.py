bc72dba7f7adae426ad8a9635ce13c0a79b6bca9
Paula Rodríguez <paular2000@uma.es>
2025-07-29 12:35:09 +0200

    Fecha de nacimiento

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,26 @@ st.write(
     "Por favor, introduzca los datos del paciente.")
 with st.form(key="registro_form"):
     nombre = st.text_input("Nombre")
-    edad = st.number_input("Edad",
-                            min_value= 0,
-                            max_value= 100)
-    profesion = st.selectbox("Elija su profesion: ",options=["Jardinero","Profesor"])
+    st.markdown("#### Fecha de nacimiento")
+    
+    dias = list(range(1, 32))
+    meses = [
+        "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+    ]
+    anios = list(range(datetime.now().year, 1900, -1))
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        dia = st.selectbox("Día", dias)
+
+    with col2:
+        mes_nombre = st.selectbox("Mes", meses)
+
+    with col3:
+        anio = st.selectbox("Año", anios)
+
+    profesion = st.selectbox("Elija su profesión:", options=["Jardinero", "Profesor"])
+
+    submit = st.form_submit_button("Enviar")
