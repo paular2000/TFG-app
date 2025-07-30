739365439d3826c7b052718da118b9d376c4e7be
Paula Rodríguez <paular2000@uma.es>
2025-07-30 13:54:12 +0200

    pantalla 2 pruebas

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,8 +71,72 @@ elif st.session_state.pantalla == 2:
       st.markdown("#### Bloque I: Comprensión oral")
       resultado_T1= st.number_input("Discriminación de fonemas: ", min_value=0, max_value=32, 
                               value=0) 
-
-
+      resultado_T2= st.number_input("Decisión léxica auditiva: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T3= st.number_input("Emparejamiento palabra hablada- dibujo: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T4= st.number_input("Repetición de palabras: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T5= st.number_input("Repetición de pseudopalabras: ", min_value=0, max_value=32, 
+                              value=0) 
 
       st.markdown("#### Bloque II: Producción oral") 
+      resultado_T6= st.number_input("Denominación de objetos: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T7= st.number_input("Denominación de acciones: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T8= st.number_input("Nombrar a definiciones: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T9= st.number_input("Fluidez verbal: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T10= st.number_input("Fluidez verbal de personajes: ", min_value=0, max_value=32, 
+                              value=0) 
+      
+      st.markdown("#### Bloque III: Lectura") 
+      resultado_T11= st.number_input("Nombrado de letras: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T12= st.number_input("Decisión léxica visual: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T13= st.number_input("Lectura de palabras: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T14= st.number_input("Lectura de pseudopalabras: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T15= st.number_input("Emparejamiento palabra escrita- dibujo: ", min_value=0, max_value=32, 
+                              value=0) 
+      
+      st.markdown("#### Bloque IV: Escritura") 
+      resultado_T16= st.number_input("Señalar la letra: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T17= st.number_input("Copia de mayúscula a minúscula: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T18= st.number_input("Denominación escrita de dibujos: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T19= st.number_input("Dictado de palabras de ortografía arbitraria: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T20= st.number_input("Dictado de pseudopalabras: ", min_value=0, max_value=32, 
+                              value=0) 
+      
+      st.markdown("#### Bloque V: Semántica") 
+      resultado_T21= st.number_input("Asociación semántica: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T22= st.number_input("Emparejamiento objeto- acción: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T23= st.number_input("Emparejamiento definición- palabra: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T24= st.number_input("Emparejamiento de sinónimos: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T25= st.number_input("Señalar el diferente: ", min_value=0, max_value=32, 
+                              value=0) 
+      
+      st.markdown("#### Bloque VI: Oraciones") 
+      resultado_T26= st.number_input("Emparejamiento oración hablada- dibujo: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T27= st.number_input("Emparejamiento oración escrita- dibujo: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T28= st.number_input("Juicios de gramaticalidad: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T29= st.number_input("Prueba de dígitos: ", min_value=0, max_value=32, 
+                              value=0) 
+      resultado_T30= st.number_input("Descripción de una lámina: ", min_value=0, max_value=32, 
+                              value=0) 
 
