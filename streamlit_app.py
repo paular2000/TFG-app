486975b021020c8fb463bfa09f9f9f0d0e9374f0
pduran24 <pabloduranp5@gmail.com>
2025-08-25 19:11:24 +0200

    prueba

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,7 @@ from pantallas import pantalla_login, pantalla_registro, pantalla_resultados
 
 
 if 'pantalla' not in st.session_state:
-
-    #Inicializo las BD
-    inicializar_BD()
-    inicializar_logopedas
-
     st.session_state.pantalla = 0
-
 if st.session_state.pantalla == 0:
     pantalla_login()
 elif st.session_state.pantalla == 1:
