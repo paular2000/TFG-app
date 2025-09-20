9883f13b16565371969cb4c715e9805ed21f2339
Paula Rodríguez <paular2000@uma.es>
2025-09-20 10:03:42 +0200

    titutlo_bienvenida

--- pantallas.py
+++ pantallas.py
@@ -76,7 +76,7 @@ def pantalla_login():
 #PAGINA PRINCIPAL DEL LOGOPEDA
 #-------------------------
 def pantalla_logopeda():
-    st.title("Logopea (nombre logopeda)")
+    st.title("Bienvenido " + st.session_state("usuario"))
 
     # bandera para mantener la lista visible entre reruns
     if 'show_pacientes' not in st.session_state:
