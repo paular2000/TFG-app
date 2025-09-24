b4face06539f0c83abccefc1a55434fdfaf67980
pduran24 <pabloduranp5@gmail.com>
2025-09-24 20:32:58 +0200

    reescrito

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,19 @@
 from ui import pantalla_login, pantalla_logopeda, pantalla_paciente, pantalla_registro, pantalla_resultados
 from core.services.logopeda_service import LogopedaService
 from core.services.paciente_service import PacienteService
+import streamlit as st
+
+
+import sys
+import os
+
+# Agregar la carpeta raíz del proyecto al PYTHONPATH
+sys.path.append(os.path.dirname(__file__))
+
+
 from bd.logopeda_repository_sheets import LogopedaRepositorySheets
 from bd.paciente_repository_sheets import PacienteRepositorySheets
-import streamlit as st
+
 
 
 
