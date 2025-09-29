36347202adcf2baea1b18eef4bf9ef77c6ce8b38
pduran24 <pabloduranp5@gmail.com>
2025-09-29 23:28:00 +0200

    pantalla_logopeda

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 
-from ui import pantalla_logopeda
+from ui import login
 
 
 
@@ -12,12 +12,12 @@ from ui import pantalla_logopeda
 def main():
 
     if "pantalla" not in st.session_state:
-        st.session_state.pantalla = 1
+        st.session_state.pantalla = 0
     
+    if st.session_state.pantalla == 0:
+        login.pantalla_login()
 
-    if st.session_state.pantalla == 1:
-        pantalla_logopeda.pantalla_logopeda()
-
+    
 
 if __name__ == "__main__":
     main()
