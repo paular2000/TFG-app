364d0382a7571979f8d0a73d4f4cb18096b6133e
pduran24 <pabloduranp5@gmail.com>
2025-09-30 18:34:48 +0200

    pacientes

--- ui/pantalla_logopeda.py
+++ ui/pantalla_logopeda.py
@@ -12,9 +12,6 @@ def pantalla_logopeda():
         st.error("❌ Error: ID de logopeda no encontrado en la sesión.")
         return
     
-    # Botón para mostrar/ocultar pacientes
-    if "show_pacientes" not in st.session_state:
-        st.session_state.show_pacientes = False
 
     col1, col2 = st.columns([1,1])
     with col1:
@@ -25,18 +22,17 @@ def pantalla_logopeda():
             st.session_state.pantalla = "registro_paciente"
             st.experimental_rerun()
     
-    if st.session_state.show_pacientes:
-        ok, pacientes = paciente_service.obtener_pacientes_por_logopeda(id_logopeda)
-        if ok and pacientes:
-            st.subheader("📋 Lista de pacientes")
-            cols = st.columns(3)  # 3 columnas por fila
+    pacientes = paciente_service.obtener_pacientes_por_logopeda(id_logopeda)
+    if pacientes:
+        st.subheader("📋 Lista de pacientes")
+        cols = st.columns(3)  # 3 columnas por fila
 
-            for i, paciente in enumerate(pacientes):
-                with cols[i % 3]:
-                    key = f"paciente_{paciente.id}"
-                    if st.button(f"{paciente.nombre} {paciente.apellidos}", key=key):
-                        st.session_state["paciente_actual_id"] = paciente.id
-                        st.session_state.pantalla = "ficha_paciente"
-                        st.experimental_rerun()
-        else:
-            st.info("No tienes pacientes registrados aún.")
+        for i, paciente in enumerate(pacientes):
+            with cols[i % 3]:
+                key = f"paciente_{paciente.id}"
+                if st.button(f"{paciente.nombre} {paciente.apellidos}", key=key):
+                    st.session_state["paciente_actual_id"] = paciente.id
+                    st.session_state.pantalla = "ficha_paciente"
+                    st.experimental_rerun()
+    else:
+        st.info("No tienes pacientes registrados aún.")
