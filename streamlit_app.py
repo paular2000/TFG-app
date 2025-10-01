02960f1a6ac1324bc22c2eb2b1632a704ebe1f1e
Pablo Durán Pérez <pablo.s420601@cesurformacion.com>
2025-10-01 08:20:02 +0200

    reseteo formulario

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,14 @@ from ui import login, pantalla_logopeda, pantalla_registro_paciente
 
 
 
-
-
-
 def main():
 
     if "pantalla" not in st.session_state:
         st.session_state.pantalla = 0
-    
+
     if st.session_state.pantalla == 0:
         login.pantalla_login()
-    elif st.session_state.pantalla == 1:
-        pantalla_logopeda.pantalla_logopeda()
-    elif st.session_state.pantalla == 3:
-        pantalla_registro_paciente.pantalla_registro_paciente()
+    
     
 
 if __name__ == "__main__":
