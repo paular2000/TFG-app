ca21d0bb46985e004b1392fde6fedd7781695f63
pduran24 <pabloduranp5@gmail.com>
2025-10-04 10:44:31 +0200

    hamburguesa

--- ui/pantalla_logopeda.py
+++ ui/pantalla_logopeda.py
@@ -4,64 +4,13 @@ from services import paciente_service
 
 def pantalla_logopeda():
 
-    st.markdown(
-    """
-    <style>
-    .menu {
-        position: fixed;
-        top: 20px;
-        left: 20px;
-        z-index: 9999; /* Forzar a estar por encima */
-    }
-    .menu input { display: none; }
-    .menu span {
-        display: block;
-        width: 30px;
-        height: 4px;
-        margin: 6px;
-        background: #000;
-        border-radius: 2px;
-        transition: 0.4s;
-    }
-    .menu-content {
-        display: none;
-        background: #f5f5f5;
-        padding: 12px;
-        border-radius: 10px;
-        margin-top: 10px;
-        box-shadow: 0 2px 6px rgba(0,0,0,0.3);
-    }
-    .menu input:checked ~ .menu-content {
-        display: block;
-    }
-    .menu-content a {
-        text-decoration: none;
-        color: #333;
-        display: block;
-        margin: 8px 0;
-        font-weight: 500;
-    }
-    .menu-content a:hover {
-        color: #007bff;
-    }
-    </style>
-
-    <div class="menu">
-        <label>
-            <input type="checkbox" id="menu-toggle"/>
-            <span></span>
-            <span></span>
-            <span></span>
-            <div class="menu-content">
-                <a href="#">🏠 Inicio</a>
-                <a href="#">👤 Perfil</a>
-                <a href="#">📋 Pacientes</a>
-            </div>
-        </label>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+    with st.sidebar:
+            st.title("Menú")
+            if st.button("Cerrar sesión"):
+                st.session_state.pantalla = 0
+                st.session_state["usuario"] = ""
+                st.session_state["id_logopeda"] = ""
+                st.rerun()
 
     
     usuario = st.session_state.get("usuario", "Usuario desconocido")
