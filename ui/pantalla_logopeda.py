f9f667bae718afa65378b4c7deee7f7b2e121672
pduran24 <pabloduranp5@gmail.com>
2025-10-04 10:43:07 +0200

    hamburguesa

--- ui/pantalla_logopeda.py
+++ ui/pantalla_logopeda.py
@@ -3,29 +3,7 @@ import streamlit as st
 from services import paciente_service
 
 def pantalla_logopeda():
-    
-    usuario = st.session_state.get("usuario", "Usuario desconocido")
-    st.title(f"Bienvenido, " +  usuario)
-    
-    
-    id_logopeda = st.session_state.get("id_logopeda")
-    
-    st.write("Id Logopeda: " + id_logopeda)
-    
-    if id_logopeda:
 
-        ok, lista_pacientes = paciente_service.obtener_pacientes_por_logopeda(id_logopeda)
-        
-        if ok and lista_pacientes:
-            st.write("Pacientes asignados:")
-            for paciente in lista_pacientes[1]:
-                st.write(f"- {paciente.nombre} {paciente.apellidos}, Edad: {paciente.edad}")
-        else:
-            st.write("No tienes pacientes asignados.")
-
-
-    
-    #menu hamburguesa
     st.markdown(
     """
     <style>
@@ -86,7 +64,27 @@ def pantalla_logopeda():
 )
 
     
-        
+    usuario = st.session_state.get("usuario", "Usuario desconocido")
+    st.title(f"Bienvenido, " +  usuario)
     
+    
+    id_logopeda = st.session_state.get("id_logopeda")
+    
+    st.write("Id Logopeda: " + id_logopeda)
+    
+    if id_logopeda:
 
+        ok, lista_pacientes = paciente_service.obtener_pacientes_por_logopeda(id_logopeda)
+        
+        if ok and lista_pacientes:
+            st.write("Pacientes asignados:")
+            for paciente in lista_pacientes[1]:
+                st.write(f"- {paciente.nombre} {paciente.apellidos}, Edad: {paciente.edad}")
+        else:
+            st.write("No tienes pacientes asignados.")
+
+
+    
+    
+    
     
