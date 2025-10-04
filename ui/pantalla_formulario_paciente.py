d283c30c4f6ddff97d00ca71fad7a87a61cd4bc5
pduran24 <pabloduranp5@gmail.com>
2025-10-04 19:02:57 +0200

    fecha_nacimiento

--- ui/pantalla_formulario_paciente.py
+++ ui/pantalla_formulario_paciente.py
@@ -19,7 +19,8 @@ def pantalla_formulario_paciente():
         ]
         anios = list(range(datetime.now().year, 1900, -1))
 
-        with st.container("Fecha de nacimiento"):
+        with st.container():
+            st.subheader("Fecha de nacimiento")
             col1, col2, col3 = st.columns(3)
             with col1:
                 dia = st.selectbox("Día", dias)
