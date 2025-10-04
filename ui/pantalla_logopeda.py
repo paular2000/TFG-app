8b81ff80cf0120a2a41b5709478c6271891045e8
pduran24 <pabloduranp5@gmail.com>
2025-10-04 10:08:54 +0200

    lista pacientes asignados

--- ui/pantalla_logopeda.py
+++ ui/pantalla_logopeda.py
@@ -11,6 +11,30 @@ def pantalla_logopeda():
     id_logopeda = st.session_state.get("id_logopeda")
     
     st.write("Id Logopeda: " + id_logopeda)
+    
+    if id_logopeda:
+
+        lista_pacientes = paciente_service.obtener_pacientes_por_logopeda(id_logopeda)
+        
+        if lista_pacientes:
+            st.write("Pacientes asignados:")
+            for paciente in lista_pacientes[1]:
+                st.write(f"- {paciente.nombre} {paciente.apellidos}, Edad: {paciente.edad}")
+        else:
+            st.write("No tienes pacientes asignados.")
+            
+    
+
+    col1 = st.columns([1,1])
+    with col1:
+        ok, pacientes = paciente_service.obtener_pacientes_por_logopeda(id_logopeda)
+        if ok and pacientes:
+            st.subheader("📋 Lista de pacientes")
+            for paciente in pacientes:
+                st.write(f"{paciente.nombre} {paciente.apellidos}")
+        else:
+            st.info("No tienes pacientes asignados.")
+    
 
     if st.button("Registrar nuevo paciente"):
         st.session_state.pantalla = 3
