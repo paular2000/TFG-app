fcc2efd13d9e2cd0008565d77bca52ee7b7d22cf
pduran24 <pabloduranp5@gmail.com>
2025-10-04 12:07:41 +0200

    Ajuste pantalla login

--- ui/login.py
+++ ui/login.py
@@ -51,78 +51,74 @@ def pantalla_login():
 
 
     st.write("")
-
     
+    usuario = st.text_input("Usuario", key="login_usuario")
+    contrasenia = st.text_input("Contraseña", type="password", key="login_contrasenia")
 
-    with st.form("login_form"):
-    
-        usuario = st.text_input("Usuario", key="login_usuario")
-        contrasenia = st.text_input("Contraseña", type="password", key="login_contrasenia")
+    if st.button("Entrar", key="btn_login"):
+        if not usuario or not contrasenia:
+            st.error("❌ Por favor, complete todos los campos.")
+        else:
+            valido, resultado = logopeda_service.validar_logopeda(usuario, contrasenia)
+            if valido:
+                st.success("Bienvenido, "+ usuario)
 
-        if st.button("Entrar", key="btn_login"):
-            if not usuario or not contrasenia:
-                st.error("❌ Por favor, complete todos los campos.")
+                
+                st.session_state["usuario"] = usuario
+
+                logopeda = logopeda_service.find_logopeda_by_user(usuario)
+                if logopeda:
+                    st.session_state["id_logopeda"] = logopeda.id # Guardo el id del logopeda en "memoria"
+                else:
+                    st.error("❌ Error al recuperar el ID del logopeda registrado.")
+                    return   
+                                
+                st.session_state.pantalla = 1 
+                st.rerun()                 
+                
             else:
-                valido, resultado = logopeda_service.validar_logopeda(usuario, contrasenia)
-                if valido:
-                    st.success("Bienvenido, "+ usuario)
+                st.error(resultado) 
 
+
+    
+    registrarse = st.button("¿Eres nuevo?")
+    if registrarse:
+        nuevo_usuario = st.text_input("Nuevo usuario", key="registro_usuario")
+        nueva_contrasenia = st.text_input("Nueva contraseña", type="password", key="registro_contrasenia")
+        confirmar_contrasenia = st.text_input("Confirmar contraseña", type="password",  key="registro_confirmar_contrasenia")
+        if st.button("Registrar", key="btn_registro"):
+            if not nuevo_usuario or not nueva_contrasenia:
+                st.error("❌ Por favor, complete todos los campos.")
+            elif nueva_contrasenia != confirmar_contrasenia:
+                st.error("❌ Las contraseñas no coinciden.")
+            else:
+                registrado, mensaje = logopeda_service.registrar_logopeda(nuevo_usuario, nueva_contrasenia)
+
+                if registrado:
+                    st.success(mensaje)
+                    st.session_state["usuario"] = nuevo_usuario
+
+                    logopeda = logopeda_service.find_logopeda_by_user(nuevo_usuario)
+                    if logopeda:
+                        st.session_state["id_logopeda"] = logopeda.id
+                    else:
+                        st.error("❌ Error al recuperar el ID del logopeda registrado.")
+                        return
                     
-                    st.session_state["usuario"] = usuario
-
-                    logopeda = logopeda_service.find_logopeda_by_user(usuario)
+                    # Obtener el objeto logopeda para guardar su ID
+                    logopeda = logopeda_service.find_logopeda_by_user(nuevo_usuario)
+                    
                     if logopeda:
                         st.session_state["id_logopeda"] = logopeda.id # Guardo el id del logopeda en "memoria"
                     else:
                         st.error("❌ Error al recuperar el ID del logopeda registrado.")
-                        return   
-                                    
-                    st.session_state.pantalla = 1 
-                    st.rerun()                 
+                        return
                     
+                    st.session_state.pantalla = 1
+                    st.rerun()
+
                 else:
-                    st.error(resultado) 
-
-
-        
-        registrarse = st.button("¿Eres nuevo?")
-        if registrarse:
-            nuevo_usuario = st.text_input("Nuevo usuario", key="registro_usuario")
-            nueva_contrasenia = st.text_input("Nueva contraseña", type="password", key="registro_contrasenia")
-            confirmar_contrasenia = st.text_input("Confirmar contraseña", type="password",  key="registro_confirmar_contrasenia")
-            if st.button("Registrar", key="btn_registro"):
-                if not nuevo_usuario or not nueva_contrasenia:
-                    st.error("❌ Por favor, complete todos los campos.")
-                elif nueva_contrasenia != confirmar_contrasenia:
-                    st.error("❌ Las contraseñas no coinciden.")
-                else:
-                    registrado, mensaje = logopeda_service.registrar_logopeda(nuevo_usuario, nueva_contrasenia)
-
-                    if registrado:
-                        st.success(mensaje)
-                        st.session_state["usuario"] = nuevo_usuario
-
-                        logopeda = logopeda_service.find_logopeda_by_user(nuevo_usuario)
-                        if logopeda:
-                            st.session_state["id_logopeda"] = logopeda.id
-                        else:
-                            st.error("❌ Error al recuperar el ID del logopeda registrado.")
-                            return
-                        
-                        # Obtener el objeto logopeda para guardar su ID
-                        logopeda = logopeda_service.find_logopeda_by_user(nuevo_usuario)
-                        
-                        if logopeda:
-                            st.session_state["id_logopeda"] = logopeda.id # Guardo el id del logopeda en "memoria"
-                        else:
-                            st.error("❌ Error al recuperar el ID del logopeda registrado.")
-                            return
-                        
-                        st.session_state.pantalla = 1
-                        st.rerun()
-
-                    else:
-                        st.error(mensaje)
+                    st.error(mensaje)
 
 
     
