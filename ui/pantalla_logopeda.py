96aa4a577a7ddf1711512794679d96b57ae6add4
pduran24 <pabloduranp5@gmail.com>
2025-10-04 18:34:00 +0200

    formulario registro paciente

--- ui/pantalla_logopeda.py
+++ ui/pantalla_logopeda.py
@@ -6,6 +6,8 @@ from PIL import Image
 
 def pantalla_logopeda():
 
+    imagen = Image.open("images/logo_y_nombre.png")
+
     with st.sidebar:
             st.title("Menú")
             if st.button("Cerrar sesión"):
@@ -14,35 +16,37 @@ def pantalla_logopeda():
                 st.session_state["id_logopeda"] = ""
                 st.rerun()
 
-    
-    usuario = st.session_state.get("usuario", "Usuario desconocido")
-        
-    imagen = Image.open("images/logo_y_nombre.png")
-    
-    id_logopeda = st.session_state.get("id_logopeda")
-    st.write("Id Logopeda: " + id_logopeda)
-    
-    if id_logopeda:
-        
-        #col1, col2, col3, col4 = st.columns([1,1,1,1])
 
-        st.image(imagen, use_container_width=True)
+    with st.container():
+            
+        usuario = st.session_state.get("usuario", "Usuario desconocido")
+            
         
-        ok, lista_pacientes = paciente_service.obtener_pacientes_por_logopeda(id_logopeda)
         
+        id_logopeda = st.session_state.get("id_logopeda")
+        st.write("Id Logopeda: " + id_logopeda)
+        
+        if id_logopeda:
+            
+            #col1, col2, col3, col4 = st.columns([1,1,1,1])
 
-        if ok and lista_pacientes:
-            st.write("Pacientes asignados:")
-            for paciente in lista_pacientes[1]:
-                st.write(f"- {paciente.nombre} {paciente.apellidos}, Edad: {paciente.edad}")
-        else:
-            st.write("No tienes pacientes asignados.")
+            st.image(imagen, use_container_width=True)
+            
+            ok, lista_pacientes = paciente_service.obtener_pacientes_por_logopeda(id_logopeda)
+            
 
-    boton_registrar_paciente = st.button("Crear nuevo paciente")
+            if ok and lista_pacientes:
+                st.write("Pacientes asignados:")
+                for paciente in lista_pacientes[1]:
+                    st.write(f"- {paciente.nombre} {paciente.apellidos}, Edad: {paciente.edad}")
+            else:
+                st.write("No tienes pacientes asignados.")
 
-    if boton_registrar_paciente:
-        st.session_state.pantalla = 2
-        st.rerun()
+        boton_registrar_paciente = st.button("Crear nuevo paciente")
+
+        if boton_registrar_paciente:
+            st.session_state.pantalla = 2
+            st.rerun()
 
 
     
