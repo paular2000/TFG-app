bbac9817c5ab4d411699867cb5478cf4cf7a8bbf
pduran24 <pabloduranp5@gmail.com>
2025-10-04 10:00:01 +0200

    elimino mostrar pacientes y su id

--- ui/pantalla_logopeda.py
+++ ui/pantalla_logopeda.py
@@ -7,13 +7,6 @@ def pantalla_logopeda():
     usuario = st.session_state.get("usuario", "Usuario desconocido")
     st.title(f"Bienvenido, " +  usuario)
     
-    id_logopeda = st.session_state.get("id_logopeda")
-    if not id_logopeda:
-        st.error("❌ Error: ID de logopeda no encontrado en la sesión.")
-        return
-    
-    
-
     
 
     
