4d18c10a29efb56d55c5011765570b9f270fb81e
pduran24 <pabloduranp5@gmail.com>
2025-10-05 13:41:15 +0200

    pantalla paciente

--- ui/pantalla_logopeda.py
+++ ui/pantalla_logopeda.py
@@ -62,6 +62,7 @@ def pantalla_logopeda():
                                     st.image(imagen_paciente, use_container_width=True)
                                     if st.button(f"{paciente.nombre} {paciente.apellidos}", key=f"btn_{paciente.id}"):
                                         st.session_state["paciente_actual_id"] = paciente.id
+                                        st.session_state.pantalla = 4
                                         st.rerun()
                 else:                  
                     st.info("No tienes pacientes asignados. Crea uno nuevo.")
