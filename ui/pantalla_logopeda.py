130ac8af072e46ddc8f3bc831ca77f14cfdf6e2a
pduran24 <pabloduranp5@gmail.com>
2025-10-08 13:10:35 +0200

    img boton

--- ui/pantalla_logopeda.py
+++ ui/pantalla_logopeda.py
@@ -63,7 +63,7 @@ def pantalla_logopeda():
                                     
 
                                     st.markdown("""
-                                        
+                                        <body>
                                         div.stButton > button:first-child {
                                             background: url ('{imagen_paciente}') no-repeat center;
                                             background-size: contain;
@@ -71,6 +71,7 @@ def pantalla_logopeda():
                                             width: 150px;
                                             border: none;
                                         }
+                                        </body>
                                         <style>
                                         div.stButton > button:first-child:hover {
                                             transform: scale(1.1);
