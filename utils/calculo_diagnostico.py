c7497559a11095471a716c65318f7e38e1fe9d81
pduran24 <pabloduranp5@gmail.com>
2025-10-13 18:20:34 +0200

    logica diagnostico

--- utils/calculo_diagnostico.py
+++ utils/calculo_diagnostico.py
@@ -3,70 +3,41 @@
 
 def determinar_diagnostico(t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17,t18,t19,t20,t21,t22,t23,t24,t25,t26,t27,t28,t29,t30):
 
+    
+
+
+    trastorno_t1 = t1 < 29
+    trastorno_t2 = t2 < 29
+    trastorno_t3 = t3 < 28
+    trastorno_t4 = t4 < 31
+    trastorno_t5 = t5 < 24
+    trastorno_t6 = t6 < 23
+    trastorno_t7 = t7 < 20
+    trastorno_t8 = t8 < 22
+    trastorno_t9 = t9 < 15
+    trastorno_t10 = t10 < 5
+    trastorno_t11 = t11 < 19
+    trastorno_t12 = t12 < 30
+    trastorno_t13 = t13 < 31
+    trastorno_t14 = t14 < 26
+    trastorno_t15 = t15 < 29
+    trastorno_t16 = t16 < 19
+    trastorno_t17 = t17 < 7
+    trastorno_t18 = t18 < 9
+    trastorno_t19 = t19 < 7
+    trastorno_t20 = t20 < 7
+    trastorno_t21 = t21 < 28
+    trastorno_t22 = t22 < 27
+    trastorno_t23 = t23 < 27
+    trastorno_t24 = t24 < 25
+    trastorno_t25 = t25 < 24
+    trastorno_t26 = t26 < 15
+    trastorno_t27 = t27 < 16
+    trastorno_t28 = t28 < 31
+    trastorno_t29 = t29 < 4
+    trastorno_t30 = t30 < 3
+
     diagnostico = "-"
-
-
-    if t1 < 29:
-        trastorno_t1 = True
-    if t2 < 29:
-        trastorno_t2 = True
-    if t3 < 28:
-        trastorno_t3 = True
-    if t4 < 31:
-        trastorno_t4 = True
-    if t5 < 24:
-        trastorno_t5 = True
-    if t6 < 23:
-        trastorno_t6 = True
-    if t7 < 20:
-        trastorno_t7 = True
-    if t8 < 22:
-        trastorno_t8 = True
-    if t9 < 15:
-        trastorno_t9 = True
-    if t10 < 5:
-        trastorno_t10 = True
-    if t11 < 19:
-        trastorno_t11 = True
-    if t12 < 30:
-        trastorno_t12 = True
-    if t13 < 31:
-        trastorno_t13 = True
-    if t14 < 26:
-        trastorno_t14 = True
-    if t15 < 29:
-        trastorno_t15 = True
-    if t16 < 19:
-        trastorno_t16 = True
-    if t17 < 7:
-        trastorno_t17 = True
-    if t18 < 9:
-        trastorno_t18 = True
-    if t19 < 7:
-        trastorno_t19 = True
-    if t20 <7:
-        trastorno_t20 = True
-    if t21 < 28:
-        trastorno_t21 = True
-    if t22 < 27:
-        trastorno_t22 = True
-    if t23 < 27:
-        trastorno_t23 = True
-    if t24 < 25:
-        trastorno_t24 = True
-    if t25 < 24:
-        trastorno_t25 = True
-    if t26 < 15:
-        trastorno_t26 = True
-    if t27 < 16:
-        trastorno_t27 = True
-    if t28 < 31:
-        trastorno_t28 = True
-    if t29 < 4:
-        trastorno_t29 = True
-    if t30 < 3:
-        trastorno_t30 = True
-
 
     if (trastorno_t6 and trastorno_t7 and trastorno_t8 and trastorno_t9 and trastorno_t3 
         and trastorno_t15 and trastorno_t23 and trastorno_t21 and trastorno_t22 and trastorno_t24 and trastorno_t25):
