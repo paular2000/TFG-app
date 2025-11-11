57e651f524599c1cfbda65b1228f27f80e226784
Pablo <pabloduranp5@gmail.com>
2025-11-11 19:16:04 +0100

    feat(sfa): Implementar Fase 1 (Modelos y Repositorio SFA) (#1)
    * Implementacion modelo EstimuloSFA
    
    * feat(sfa): Finalizar implementación de Fase 1

--- models/models.py
+++ models/models.py
@@ -25,4 +25,18 @@ class Paciente:
     habito_lector: str
     aficiones: List[str]
     diagnostico: str
+
+@dataclass
+class EstimuloSFA:
+    id: str
+    solucion: str # la respuesta correcta (columna estimulo)
+    url: str
+    categoria: str
+    uso: str
+    accion: str
+    propiedades: str
+    localizacion: str
+    mostrar: str
+
+
     
